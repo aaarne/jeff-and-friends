8fbf0bf7c194380f75c178199413d23651de8f5f
Arne Sachtler <arne.sachtler@gmail.com>
2022-01-22 22:44:35 +0100

    Add java

---
+++ app/src/main/java/de/kaffee/flora4future/OldActivity.kt
@@ -0,0 +1,28 @@
+package de.kaffee.flora4future
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.hardware.camera2.*
+import android.content.Intent
+import android.provider.MediaStore
+import android.content.ActivityNotFoundException
+import android.util.Log
+
+class OldActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        dispatchTakePictureIntent()
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, 1)
+        } catch (e: ActivityNotFoundException) {
+            Log.e("Error", "Can not open camera!")
+        }
+    }
+
+}
