d5da8bf9176c80041fa75d4c8781783aceb98aab
Arne Sachtler <arne.sachtler@gmail.com>
2022-01-22 18:02:59 +0100

    New initial commit

---
+++ app/src/main/java/de/kaffee/flora4future/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.kaffee.flora4future
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
